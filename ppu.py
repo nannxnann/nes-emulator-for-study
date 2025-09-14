7e9dffae8a3ec936d40d92bffbc7c960ef46deb0
nannxnann <nannxnann@gmail.com>
2025-09-14 13:22:57 +0800

    registers of ppu
    several update for ppu

--- ppu.py
+++ ppu.py
@@ -8,7 +8,57 @@ chrom = []
 # to be implemented
 class PPU:
     reg = 0
-
+    reg_internal_vramaddr # 16bit address
+    reg_internal_readbuffer # only changed after ppudata read
+    reg_internal_v = 0  # 
+    reg_internal_t = 0  # 
+    reg_internal_x = 0  # 
+    reg_internal_w = 0  # boolean, vramdata first byte or second
+    reg_ctrl = 0     # $2000 write
+    reg_mask = 0     # $2001 write
+    reg_status = 0   # $2002 read
+    reg_oamaddr = 0  # $2003 write
+    reg_oamdata = 0  # $2004 read/write
+    reg_scroll = 0   # $2005 write
+    reg_vramaddr = 0 # $2006 write
+    reg_vramdata = 0 # $2007 read/write
+    reg_dma = 0      # $4014 write
+    def write_vramaddr(self, data):
+        data &= 0b11111111
+        if reg_internal_w == 0:
+            reg_internal_vramaddr &= 0b11111111
+            reg_internal_vramaddr |= (data << 8)
+        else:
+            reg_internal_vramaddr &= 0b1111111100000000
+            reg_internal_vramaddr |= data
+            
+    def read_vramdata(self):
+        read_addr = self.reg_internal_vramaddr
+        addr_inc = True if (self.reg_ctrl&0b00000100) else False
+        if addr_inc:
+            self.reg_internal_vramaddr+=32
+        else:
+            self.reg_internal_vramaddr+=1
+        self.reg_internal_vramaddr &= 0x3fff
+        if read_addr < 0x3000:
+            ret = reg_internal_readbuffer
+            reg_internal_readbuffer = ppuMemory[read_addr]
+        elif read_addr < 0x3eff:
+            print("invlid, why?")
+        elif read_addr < 0x3eff:
+            print("pallete")
+            ret = ppuMemory[read_addr]
+        else:
+            print("unexpected memory access")
+        return ret
+    def write_vramdata(self, data):
+        data &= 0b11111111
+        if reg_internal_w == 0:
+            reg_internal_vramaddr &= 0b11111111
+            reg_internal_vramaddr |= (data << 8)
+        else:
+            reg_internal_vramaddr &= 0b1111111100000000
+            reg_internal_vramaddr |= data
 #todo: 目前完成 render chrom (但好像有bug need fix)
 # 先嘗試把 ROM 內的 pattern 打印出來
 if __name__=="__main__":
